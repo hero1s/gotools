851385fb73107523b250bb7adf81cb368ad82abb
toney <yinguohua@lewanhuyu.com>
2019-12-24 00:05:00 +0800

    Update alipay_sdk.go

--- gopay/alipay_sdk.go
+++ gopay/alipay_sdk.go
@@ -11,7 +11,7 @@ import (
 
 //UserCertifyOpenInitialize 身份认证初始化服务 https://docs.open.alipay.com/api_2/alipay.user.certify.open.initialize
 func UserCertifyOpenInitialize(realName, identity, returnUrl string) (string, error) {
-	log.Info("========== UserCertifyOpenInitialize ==========")
+	log.Debug("========== UserCertifyOpenInitialize ==========")
 	var p = gopay.UserCertifyOpenInitialize{}
 	p.OuterOrderNo = utils.GenStringUUID()
 	p.BizCode = gopay.K_CERTIFY_BIZ_CODE_FACE
@@ -22,25 +22,25 @@ func UserCertifyOpenInitialize(realName, identity, returnUrl string) (string, er
 	p.MerchantConfig.ReturnURL = returnUrl
 
 	pxx, _ := json.Marshal(p)
-	log.Info("身份初始化验证参数:%v", string(pxx))
+	log.Debug("身份初始化验证参数:%v", string(pxx))
 
 	rsp, err := AliPayClient.UserCertifyOpenInitialize(common.ChangeStructToJsonMap(p))
 	if err != nil {
 		log.Error("身份验证初始化失败:%v", err)
 		return "", err
 	}
-	log.Info("初始化身份验证返回:%#v", rsp)
+	log.Debug("初始化身份验证返回:%#v", rsp)
 	if rsp.Content.Code != "10000" {
-		log.Info("初始化认证不成功:%v,%v", rsp.Content.Msg, rsp.Content.SubMsg)
+		log.Debug("初始化认证不成功:%v,%v", rsp.Content.Msg, rsp.Content.SubMsg)
 		return "", errors.New("认证初始化不成功")
 	}
-	log.Info(rsp.Content.CertifyId)
+	log.Debug(rsp.Content.CertifyId)
 	return rsp.Content.CertifyId, nil
 }
 
 //UserCertifyOpenCertify 身份认证开始认证 https://docs.open.alipay.com/api_2/alipay.user.certify.open.certify
 func UserCertifyOpenCertify(certifyId string) (string, error) {
-	log.Info("========== UserCertifyOpenCertify ==========")
+	log.Debug("========== UserCertifyOpenCertify ==========")
 	body := map[string]interface{}{
 		"certify_id": certifyId,
 	}
@@ -48,13 +48,13 @@ func UserCertifyOpenCertify(certifyId string) (string, error) {
 	if err != nil {
 		log.Error("%#v 身份认证错误:%v", body, err)
 	}
-	log.Info("身份认证返回值:%#v", string(rsp))
+	log.Debug("身份认证返回值:%#v", string(rsp))
 	return string(rsp), err
 }
 
 // UserCertifyOpenQuery 身份认证记录查询 https://docs.open.alipay.com/api_2/alipay.user.certify.open.query/
 func UserCertifyOpenQuery(certifyId string) (rsp gopay.UserCertifyOpenQueryRsp, err error) {
-	log.Info("========== UserCertifyOpenQuery ==========")
+	log.Debug("========== UserCertifyOpenQuery ==========")
 	var p = gopay.UserCertifyOpenQuery{}
 	p.CertifyId = certifyId
 	rsp, err = AliPayClient.UserCertifyOpenQuery(common.ChangeStructToJsonMap(p))
@@ -65,6 +65,6 @@ func UserCertifyOpenQuery(certifyId string) (rsp gopay.UserCertifyOpenQueryRsp, 
 	if rsp.Content.Code != "10000" {
 		log.Error("验证信息不成功:", rsp.Content.Msg, rsp.Content.SubMsg)
 	}
-	log.Info("身份认证记录查询:%#v", rsp.Content)
+	log.Debug("身份认证记录查询:%#v", rsp.Content)
 	return
 }
