a5d79d46024841a7daa72b7f3bf96c7b99414124
toney <yinguohua@lewanhuyu.com>
2020-02-17 11:44:59 +0800

    添加移除字符串

--- utils/utils.go
+++ utils/utils.go
@@ -171,6 +171,21 @@ func RemoveElementUint64(nums []uint64, val uint64) int {
 	}
 	return len(nums)
 }
+func RemoveElementString(nums []string, val string) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	index := 0
+	for ; index < len(nums); {
+		if nums[index] == val {
+			nums = append(nums[:index], nums[index+1:]...)
+			continue
+		}
+		index++
+	}
+	return len(nums)
+}
+
 //安全执行异步函数
 func SafeGoroutine(f func()) {
 	go func() {
