43060eac2c4b96ce9117df959ad773a1df0d100c
toney <yinguohua@lewanhuyu.com>
2020-05-02 20:00:35 +0800

    添加日志错误打印

--- log/log_darwin.go
+++ log/log_darwin.go
@@ -27,10 +27,12 @@ func InitLog(runMode string, dirs ...string) *logs.BeeLogger {
 			oldMask := syscall.Umask(0)
 			err = os.Mkdir(logDir, 0755)
 			if err != nil {
+				println(err.Error(),logDir)
 				panic(err)
 			}
 			f, err := os.Create(logFile)
 			if err != nil {
+				println(err.Error(),logDir)
 				panic(err)
 			}
 			f.Close()
@@ -39,6 +41,7 @@ func InitLog(runMode string, dirs ...string) *logs.BeeLogger {
 			if !isPathExist(logFile) {
 				f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
 				if err != nil {
+					println(err.Error(),logFile)
 					panic(err)
 				}
 				f.Close()
@@ -47,6 +50,7 @@ func InitLog(runMode string, dirs ...string) *logs.BeeLogger {
 		configs := `{"filename":"` + logFile + `","separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`
 		err = DefaultLog.SetLogger(logs.AdapterMultiFile, configs)
 		if err != nil {
+			println(err.Error())
 			panic(err)
 		}
 	}
--- log/log_windows.go
+++ log/log_windows.go
@@ -25,21 +25,23 @@ func InitLog(runMode string, dirs ...string) *logs.BeeLogger {
 		if !isPathExist(logDir) {
 			err = os.Mkdir(logDir, 0755)
 			if err != nil {
+				println(err.Error(),logDir)
 				panic(err)
 			}
 			f, err := os.Create(logFile)
 			if err != nil {
+				println(err.Error(),logFile)
 				panic(err)
 			}
 			f.Close()
 		} else {
 			f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
 			if err != nil {
+				println(err.Error(),logFile)
 				panic(err)
 			}
 			f.Close()
 		}
-		logFile = "log/log.txt"
 		configs := `{"filename":"` + logFile + `","separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`
 		err = DefaultLog.SetLogger(logs.AdapterMultiFile, configs)
 		if err != nil {
