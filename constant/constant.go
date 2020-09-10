6d0d7b9ee8ff92a8a0f4537d784eae088508f342
toney <yinguohua@lewanhuyu.com>
2020-09-10 10:37:39 +0800

    添加缓存函数，常量定义

---
+++ constant/constant.go
@@ -0,0 +1,9 @@
+package constant
+
+//常用常量
+const (
+	SECOND_ONE_MIN  = 60                   // 一分钟
+	SECOND_ONE_HOUR = 60 * SECOND_ONE_MIN  // 一小时
+	SECOND_ONE_DAY  = 24 * SECOND_ONE_HOUR // 一天
+	SECOND_ONE_WEEK = 7 * SECOND_ONE_DAY   // 一周
+)
