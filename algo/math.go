972564843e40643b7ea3cf4a8e69651f7d10be07
toney <yinguohua@lewanhuyu.com>
2020-09-15 16:35:51 +0800

    Update math.go

--- algo/math.go
+++ algo/math.go
@@ -51,3 +51,16 @@ func RemoveDuplicateStr(list []string) []string {
 	return x
 }
 
+func Min(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Max(x, y int64) int64 {
+	if x > y {
+		return x
+	}
+	return y
+}
